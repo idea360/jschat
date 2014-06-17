c1c754bc2e422524348b0df77855df3f5935b6e6
Chris <idea360@hotmail.com>
2014-06-16 20:56:49 -0700

    adding data validations to prevent crashes

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,25 @@ module.exports = function Route(app){
   });
   
   app.io.route("new_user", function(req){
-    users[req.sessionID] = {name: req.data.name};
-    console.log(users);
-    req.io.emit("get_active_users", {users: users});
-    req.io.broadcast("new_user_joined", {user: req.data.name, session: req.sessionID});
+    if( req.data.name.length < 2){
+      req.io.emit('bad_user')
+    }
+    else{
+      users[req.sessionID] = {name: req.data.name};
+      console.log(users);
+      req.io.emit("get_active_users", {users: users});
+      req.io.broadcast("new_user_joined", {user: req.data.name, session: req.sessionID});
+    }
   });
 
   app.io.route("chatSubmit", function(req){
-    app.io.broadcast("addChat", {name: users[req.sessionID].name, message: req.data.message })
+    console.log(users[req.sessionID])
+    if (users[req.sessionID] === undefined){
+      req.io.emit("bad_user");        
+    }
+    else{
+      app.io.broadcast("addChat", {name: users[req.sessionID].name, message: req.data.message })
+    }
   });
 
   app.io.route("disconnect", function(req){
@@ -26,4 +37,5 @@ module.exports = function Route(app){
     }
     app.io.broadcast("user_disconnected", {sessionid: req.sessionID} );
   });
+
 }
